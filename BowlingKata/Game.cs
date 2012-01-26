50c8e59491f415f44fde072bbc18c414061db8af
V5 Core Dev <collv5coredev@algorithmics.com>
2012-01-26 13:47:23 -0500

    First 5 Tests.  Code for first 4

---
+++ BowlingKata/Game.cs
@@ -0,0 +1,52 @@
+﻿namespace BowlingKata {
+	public class Game {
+		private int[] rolls = new int[21];
+		private int currentRoll = 0;
+
+		public void Roll(int pins) {
+			rolls[currentRoll++] = pins;
+		}
+
+		public int Score() {
+			int score = 0;
+			int frameIndex = 0;
+			for (int frame = 0; frame < 10; frame++) {
+				if (IsStrike(frameIndex))
+				{
+					score += 10 + strikeBonus(frameIndex);
+					frameIndex++;
+				}
+				else if (IsSpare(frameIndex)) {
+					score += 10 + spareBonus(frameIndex);
+					frameIndex += 2;
+				}
+				else {
+					score += sumOfBallsInFrame(frameIndex);
+					frameIndex += 2;
+				}
+			}
+			return score;
+		}
+
+		private bool IsStrike(int frameIndex) {
+			return rolls[frameIndex] == 10;
+		}
+
+		private int sumOfBallsInFrame(int frameIndex) {
+			return rolls[frameIndex] + rolls[frameIndex + 1];
+		}
+
+		private int spareBonus(int frameIndex) {
+			return rolls[frameIndex + 2];
+		}
+
+		private int strikeBonus(int frameIndex) {
+			return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+		}
+
+		private bool IsSpare(int frameIndex) {
+			return rolls[frameIndex] +
+						 rolls[frameIndex + 1] == 10;
+		}
+	}
+}
---
+++ BowlingKata/GameTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace BowlingKata {
+	[NUnit.Framework.TestFixture]
+	public class GameTest {
+		private Game g;
+
+		[SetUp]
+		public void SetupGame() {
+			g = new Game();
+		}
+
+		[Test]
+		public void GutterGame() {
+			RollGame(20, 0);
+			Assert.AreEqual(0, g.Score());
+		}
+
+		[Test]
+		public void TestAllOnes() {
+			RollGame(20, 1);
+			Assert.AreEqual(20, g.Score());
+		}
+
+		[Test]
+		public void TestSpare() {
+			RollSpare();
+			g.Roll(3);	
+			RollGame(17, 0);
+			Assert.AreEqual(16, g.Score());
+		}
+
+		[Test]
+		public void TestStrike() {
+			RollStrike();
+			g.Roll(3);
+			g.Roll(4);
+			RollGame(16, 0);
+			Assert.AreEqual(24, g.Score());
+		}
+
+		[Test]
+		public void TestPerfectGame() {
+			RollGame(12, 10);
+			Assert.AreEqual(300, g.Score());
+		}
+
+		private void RollStrike() {
+			g.Roll(10);
+		}
+
+		private void RollSpare() {
+			g.Roll(5);
+			g.Roll(5);
+		}
+
+		private void RollGame(int rolls, int pins) {
+			for (int i = 0; i < rolls; i++) {
+				g.Roll(pins);
+			}
+		}
+	}
+}
